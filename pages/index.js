cdbadfff2c40c3ea6a49272ad84cfd424af86d60
Cephas Osayi <cephasosayi@gmail.com>
2022-06-05 09:17:02 +0100

    'commit'

---
+++ pages/index.js
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+import Featured from '../components/Featured';
+import ProductList from '../components/productList';
+
+
+
+export default function Home() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Pizza App </title>
+        <meta name="description" content="Best Pizza app in town" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+     <div>
+     <Featured />
+     
+     <ProductList />
+     </div>
+
+    </div>
+  )
+}
